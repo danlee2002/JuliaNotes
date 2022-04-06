ee1f96ce304b221de23ffb4c24dc8307c825547d
danlee2002 <danlee@gatech.edu>
2022-04-05 21:56:05 -0400

    updates data structure

--- datastructures.jl
+++ datastructures.jl
@@ -25,8 +25,28 @@ num = [i for i in 1:16]
 println(num)
 #In many programming languages, arrays are pointers to location in memories, we can pass memory location and modify file
 mat = [i for i in 1:15]
+#This may be problematic if we just want a copy of array and not memory value
 ref = mat
+refdeep = copy(mat)
 ref[1] = 0 
+#Note, sometimes we may want to make copy an arraying containing arrays, in which we would need to use deep copy
 println(ref)
+println(refdeep)
 
+#Tuples, fixed size grouping of values that may be of same type or not
+#Tuples can be declared and intialize in the following manner
+a = (1,2,3)
+b = 1,2,3 
 
+#Applications of Tuples
+#In a sense, Tuples can let us return many values 
+function getTriple(x)
+    return x, x * 2, x * 3
+end 
+#Named tuple
+namedTuple1 = (a = 1, b = "hello")
+namedTuple1[:b] #Will return b
+#Dictionaries, a mapping type
+test = Dict("a"=> 1, "b" => 2)
+#Returns value associated with key
+test["a"]
