43547e21e18c6abf51e9fcaa9700c32e197a4301
danlee2002 <danlee@gatech.edu>
2022-04-05 21:31:45 -0400

    updates datastruct

--- datastructures.jl
+++ datastructures.jl
@@ -9,7 +9,24 @@ typeof(a)
 #An important thing to note is that Julia indexing starts at 1
 mat1 = [1 0 0; 0 1 0; 0 0 1]
 println(mat1)
-#There are times we may need to initialize a 3d array 
-
+#There are times we may need to initialize a 3d array, there is no formal convention for this, so we tend to initialize an array of zeros
+threeDimension = zeros(3,3,3)
+#Slices provide a convenient method of accessing subsets of an Array
+a = [1, 2, 3, 4, 5, 6]
+#The notation arrayName[a:b] where a,b are indices prints out all values between a and b inclusive
+println(a[2:3])
+mat1 = reshape([i for i in 1:16],4,4)
+#We can use slice notation to access subset of matrices
+mat2 = mat1[2:3, 2:3]
+println(mat1)
+println(mat2)
+#Side note, we can use list declaration 
+num = [i for i in 1:16]
+println(num)
+#In many programming languages, arrays are pointers to location in memories, we can pass memory location and modify file
+mat = [i for i in 1:15]
+ref = mat
+ref[1] = 0 
+println(ref)
 
 
